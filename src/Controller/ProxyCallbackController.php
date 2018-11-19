8c3dabc8b5a3d0add32db783c4dbd8ce0e8b3379
Voidtek <vdacosta@voidtek.com>
2018-11-19 17:49:51 +0100

    OPENEUROPA-1299: Add Kernel test for duplicate request.

--- src/Controller/ProxyCallbackController.php
+++ src/Controller/ProxyCallbackController.php
@@ -5,6 +5,11 @@ declare(strict_types = 1);
 namespace Drupal\oe_authentication\Controller;
 
 use Drupal\cas\Controller\ProxyCallbackController as CASProxyCallbackController;
+use Drupal\cas\Service\CasHelper;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -13,21 +18,57 @@ use Symfony\Component\HttpFoundation\Response;
 class ProxyCallbackController extends CASProxyCallbackController {
 
   /**
+   * The configuration factory.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  protected $configFactory;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Database\Connection $database_connection
+   *   The database service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The Symfony request stack.
+   * @param \Drupal\cas\Service\CasHelper $cas_helper
+   *   The CasHelper.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
+   *   The Config Factory.
+   */
+  public function __construct(Connection $database_connection, RequestStack $request_stack, CasHelper $cas_helper, ConfigFactoryInterface $configFactory) {
+    parent::__construct($database_connection, $request_stack, $cas_helper);
+    $this->configFactory = $configFactory;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database'),
+      $container->get('request_stack'),
+      $container->get('cas.helper'),
+      $container->get('config.factory')
+    );
+  }
+
+  /**
    * Route callback for the ProxyGrantingTicket information.
    *
    * This function stores the incoming PGTIOU and pgtId parameters so that
    * the incoming response from the EULogin Server can be looked up.
    */
   public function callback() {
-    $config = \Drupal::configFactory()->getEditable('oe_authentication.settings');
-    if ('cas' === $config->get('protocol')) {
+    $config = $this->configFactory->get('oe_authentication.settings');
+    if ($config->get('protocol') === 'cas') {
       parent::callback();
     }
 
     $current_request = $this->requestStack->getCurrentRequest();
 
     // Check the method of the request.
-    if ('POST' !== $current_request->getRealMethod()) {
+    if ($current_request->getRealMethod() !== 'POST') {
       return new Response(t('The Request should use the POST method'), 400);
     }
 
@@ -38,6 +79,10 @@ class ProxyCallbackController extends CASProxyCallbackController {
     // If either is not present, inform EULogin Server of an error.
     if (!isset($pgt_id) && !isset($pgt_iou)) {
       return new Response(t('Missing necessary parameters'), 400);
+    }
+
+    if ($this->checkPgtMapping($pgt_iou, $pgt_id)) {
+      return new Response(t('Parameters already use'), 400);
     }
 
     // Store the pgtIou and pgtId in the database for later use.
@@ -53,6 +98,27 @@ class ProxyCallbackController extends CASProxyCallbackController {
     $response->headers->set('Content-Type', 'xml');
 
     return $response;
+  }
+
+  /**
+   * Check if the pgtIou and pgtId are mapped in the database.
+   *
+   * @param string $pgt_iou
+   *   The pgtIou from CAS Server.
+   * @param string $pgt_id
+   *   The pgtId from the CAS server.
+   *
+   * @return bool
+   *   Return TRUE is present in the database.
+   */
+  protected function checkPgtMapping($pgt_iou, $pgt_id) {
+    $result = $this->connection->select('cas_pgt_storage')
+      ->fields('cas_pgt_storage', ['pgt_iou', 'pgt'])
+      ->condition('pgt_iou', $pgt_iou)
+      ->condition('pgt', $pgt_id)
+      ->execute()->fetchAll();
+
+    return count($result) > 0 ? TRUE : FALSE;
   }
 
 }
--- tests/Kernel/ProxyCallbackTest.php
+++ tests/Kernel/ProxyCallbackTest.php
@@ -45,16 +45,19 @@ class ProxyCallbackTest extends KernelTestBase {
     $kernel = $this->container->get('http_kernel');
     $proxy_callback_url = Url::fromRoute('cas.proxyCallback')->toString();
 
+    // Check the method.
     $test_request = Request::create($proxy_callback_url, 'GET');
     $response = $kernel->handle($test_request);
     $this->assertEquals(400, $response->getStatusCode());
     $this->assertEquals('The Request should use the POST method', $response->getContent());
 
+    // Check the parameters.
     $test_request = Request::create($proxy_callback_url, 'POST');
     $response = $kernel->handle($test_request);
     $this->assertEquals(400, $response->getStatusCode());
     $this->assertEquals('Missing necessary parameters', $response->getContent());
 
+    // Check a good request.
     $test_request = Request::create($proxy_callback_url, 'POST');
     $test_request->request->set('pgtId', 'foo');
     $test_request->request->set('pgtIou', 'bar');
@@ -62,6 +65,14 @@ class ProxyCallbackTest extends KernelTestBase {
     $this->assertEquals(200, $response->getStatusCode(), $response->getContent());
     $xml = '<?xml version="1.0" encoding="UTF-8"?><proxySuccess xmlns="https://localhost"/>';
     $this->assertEquals($xml, $response->getContent());
+
+    // Check a duplicate.
+    $test_request = Request::create($proxy_callback_url, 'POST');
+    $test_request->request->set('pgtId', 'foo');
+    $test_request->request->set('pgtIou', 'bar');
+    $response = $kernel->handle($test_request);
+    $this->assertEquals(400, $response->getStatusCode(), $response->getContent());
+    $this->assertEquals('Parameters already use', $response->getContent());
   }
 
 }
