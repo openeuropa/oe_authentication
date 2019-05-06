bbd8e30d6edac2f493b9aec214d2b2be07f6e99d
Imanol Eguskiza <ier@opendeusto.es>
2019-05-06 11:23:37 +0200

    OPENEUROPA-1837: Replace CAS references with EU Login.

--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -47,6 +47,14 @@ class RouteSubscriber extends RouteSubscriberBase {
         '_controller' => '\Drupal\oe_authentication\Controller\ProxyCallbackController::callback',
       ]);
     }
+
+    // Replace default cas login route with eulogin one.
+    $route = $collection->get('cas.login');
+    $route->setPath('/eulogin');
+
+    // Replace route title for the Bulk Add CAS Users route.
+    $route = $collection->get('cas.bulk_add_cas_users');
+    $route->setDefault('_title', 'Bulk Add EU Login Users');
   }
 
 }
