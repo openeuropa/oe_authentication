0aa3928095ee57cc0463dc286205967c12405d22
Claudiu Cristea <clau.cristea@gmail.com>
2019-06-04 17:08:53 +0300

    Issue #83: Expose the new config in the UI.

--- src/Form/AuthenticationSettingsForm.php
+++ src/Form/AuthenticationSettingsForm.php
@@ -4,8 +4,11 @@ declare(strict_types = 1);
 
 namespace Drupal\oe_authentication\Form;
 
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\RouteBuilderInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Settings form for module.
@@ -16,6 +19,36 @@ class AuthenticationSettingsForm extends ConfigFormBase {
    * Name of the config being edited.
    */
   const CONFIG_NAME = 'oe_authentication.settings';
+
+  /**
+   * The route builder service.
+   *
+   * @var \Drupal\Core\Routing\RouteBuilderInterface
+   */
+  protected $routeBuilder;
+
+  /**
+   * Constructs a new form instance.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The factory for configuration objects.
+   * @param \Drupal\Core\Routing\RouteBuilderInterface $route_builder
+   *   The route builder service.
+   */
+  public function __construct(ConfigFactoryInterface $config_factory, RouteBuilderInterface $route_builder) {
+    parent::__construct($config_factory);
+    $this->routeBuilder = $route_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('router.builder')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -53,6 +86,11 @@ class AuthenticationSettingsForm extends ConfigFormBase {
       '#title' => $this->t('Application available ticket types'),
       '#default_value' => $this->config(static::CONFIG_NAME)->get('ticket_types'),
     ];
+    $form['redirect_user_register_route'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Redirect user registration route to EU Login'),
+      '#default_value' => $this->config(static::CONFIG_NAME)->get('redirect_user_register_route'),
+    ];
     return parent::buildForm($form, $form_state);
   }
 
@@ -60,14 +98,23 @@ class AuthenticationSettingsForm extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $original_redirect_user_register_route = $this->config(static::CONFIG_NAME)
+      ->get('redirect_user_register_route');
+
     $this->config(static::CONFIG_NAME)
       ->set('protocol', $form_state->getValue('protocol'))
       ->set('register_path', $form_state->getValue('register_path'))
       ->set('validation_path', $form_state->getValue('validation_path'))
       ->set('assurance_level', $form_state->getValue('assurance_level'))
       ->set('ticket_types', $form_state->getValue('ticket_types'))
+      ->set('redirect_user_register_route', $form_state->getValue('redirect_user_register_route'))
       ->save();
     parent::submitForm($form, $form_state);
+
+    // Rebuild the routes if the redirect user register config has changed.
+    if ($original_redirect_user_register_route != $form_state->getValue('redirect_user_register_route')) {
+      $this->routeBuilder->rebuild();
+    }
   }
 
   /**
